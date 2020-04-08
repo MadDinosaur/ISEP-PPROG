a2398d942a181999b04cfb42e3fe0538ca96a68b
1181626 <1181626@isep.ipp.pt>
2020-04-08 22:32:32 +0100

    Criação da classe Automóvel

---
+++ 1dm_tp2_1181626_1191507/src/main/java/com/mycompany/dm_tp2_1181626_1191507/Automovel.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.dm_tp2_1181626_1191507;
+
+/**
+ *
+ * @author Francisco
+ */
+public class Automovel {
+    String a;
+}
