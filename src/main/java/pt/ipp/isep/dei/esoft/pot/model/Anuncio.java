dce16bef63dbfb2f83f847fc7d4d938220722634
Barbara Pinto <bdianapinto@outlook.com>
2020-05-11 16:20:44 +0100

    Adição de getters e setters, novos atributos Colaborador e ProcessoSeriacao.

--- src/main/java/pt/ipp/isep/dei/esoft/pot/model/Anuncio.java
+++ src/main/java/pt/ipp/isep/dei/esoft/pot/model/Anuncio.java
@@ -1,8 +1,10 @@
 package pt.ipp.isep.dei.esoft.pot.model;
 
 import java.util.Date;
-import java.util.List;
 
+//NOTAS
+//- Getters e setters de Dates não estão definidos por composição
+//- Completar construtores
 public class Anuncio {
 
     /**
@@ -37,6 +39,14 @@ public class Anuncio {
      * O tipo de regimento do anúncio
      */
     private TipoRegimento tipoRegimento;
+    /**
+     * O Colaborador que publica o anúncio
+     */
+    private Colaborador colab;
+    /**
+     * O ProcessoSeriacao criado pelo anúncio
+     */
+    private ProcessoSeriacao ps;
 
     /**
      * Cria um objeto da classe Anuncio
@@ -59,18 +69,156 @@ public class Anuncio {
         this.dtFimCandidatura = dtFimCandidatura;
         this.dtInicioSeriacao = dtInicioSeriacao;
         this.dtFimSeriacao = dtFimSeriacao;
-        
+
         this.listaCandidaturas = new ListaCandidaturas();
         this.tipoRegimento = new TipoRegimento();
     }
 
     /**
+     * Retorna a data de início do período de publicitação
+     *
+     * @return a data de início do período de publicitação, em formato Date
+     */
+    public Date getDtInicioPublicitacao() {
+        return dtInicioPublicitacao;
+    }
+
+    /**
+     * Modifica a data de início do período de publicitação
+     *
+     * @param dtInicioPublicitacao a data de início do período de publicitação
+     */
+    public void setDtInicioPublicitacao(Date dtInicioPublicitacao) {
+        this.dtInicioPublicitacao = dtInicioPublicitacao;
+    }
+
+    /**
+     * Retorna a data de fim do período de publicitação
+     *
+     * @return Date: a data de fim do período de publicitação, em formato Date
+     */
+    public Date getDtFimPublicitacao() {
+        return dtFimPublicitacao;
+    }
+
+    /**
+     * Modifica a data de fim do período de publicitação
+     *
+     * @param dtFimPublicitacao a data de fim do período de publicitação, em
+     * formato Date
+     */
+    public void setDtFimPublicitacao(Date dtFimPublicitacao) {
+        this.dtFimPublicitacao = dtFimPublicitacao;
+    }
+
+    /**
+     * Retorna a data de início do período de apresentação de candidaturas
+     *
+     * @return a data de início das candidaturas, em formato Date
+     */
+    public Date getDtInicioCandidatura() {
+        return dtInicioCandidatura;
+    }
+
+    /**
+     * Modifica a data de início do período de apresentação de candidaturas
+     *
+     * @param dtInicioCandidatura a data de início das candidaturas
+     */
+    public void setDtInicioCandidatura(Date dtInicioCandidatura) {
+        this.dtInicioCandidatura = dtInicioCandidatura;
+    }
+
+    /**
+     * Retorna a data de fim do período de apresentação de candidaturas
+     *
+     * @return a data de fim das candidaturas, em formato Date
+     */
+    public Date getDtFimCandidatura() {
+        return dtFimCandidatura;
+    }
+
+    /**
+     * Modifica a data de fim do período de apresentação de candidaturas
+     *
+     * @param dtFimCandidatura a data de fim das candidaturas
+     */
+    public void setDtFimCandidatura(Date dtFimCandidatura) {
+        this.dtFimCandidatura = dtFimCandidatura;
+    }
+
+    /**
+     * Retorna a data de início do período de seriação
+     *
+     * @return a data de início da seriação, em formato Date
+     */
+    public Date getDtInicioSeriacao() {
+        return dtInicioSeriacao;
+    }
+
+    /**
+     * Modfica a data de início do período de seriação
+     *
+     * @param dtInicioSeriacao a data de início da seriação
+     */
+    public void setDtInicioSeriacao(Date dtInicioSeriacao) {
+        this.dtInicioSeriacao = dtInicioSeriacao;
+    }
+
+    /**
+     * Retorna a data de fim do período de seriação
+     *
+     * @return a data de fim da seriação, em formato Date
+     */
+    public Date getDtFimSeriacao() {
+        return dtFimSeriacao;
+    }
+
+    /**
+     * Modfica a data de fim do período de seriação
+     *
+     * @param dtFimSeriacao a data de fim da seriação
+     */
+    public void setDtFimSeriacao(Date dtFimSeriacao) {
+        this.dtFimSeriacao = dtFimSeriacao;
+    }
+
+    /**
+     * Retorna uma cópia do tipo de regimento do anúncio
+     *
+     * @return o TipoRegimento
+     */
+    public TipoRegimento getTipoRegimento() {
+        return new TipoRegimento(tipoRegimento);
+    }
+
+    /**
+     * Retorna uma cópia do colaborador que publicou o anúncio
+     *
+     * @return o Colaborador
+     */
+    public Colaborador getColaborador() {
+        return new Colaborador(colab);
+    }
+
+    /**
      * Retorna uma cópia da lista de candidaturas recebidas para o anúncio
+     *
+     * @return a ListaCandidaturas do anúncio
      */
     public ListaCandidaturas getListaCandidaturas() {
-        return new ListaCandidaturas(listaCandidaturas);
+        return new ListaCandidaturas(this.listaCandidaturas);
     }
-    
+
+    /**
+     * Retorna uma cópia do processo de seriação criado pelo anúncio
+     *
+     * @return o ProcessoSeriacao do anúncio
+     */
+    public ProcessoSeriacao getProcessoSeriacao() {
+        return new ProcessoSeriacao(this.ps);
+    }
+
     public void novoProcessoSeriacao(Colaborador colab) {
     }
 
@@ -82,4 +230,5 @@ public class Anuncio {
 
     private void setProcessoSeriacao(ProcessoSeriacao ps) {
     }
+
 }
